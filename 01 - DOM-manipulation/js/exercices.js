98ec093491c8ea4f20d0b3474668c388ed46cfbe
Tomasspit2 <Tomas.p.spit1@gmail.com>
2023-09-12 12:27:10 +0200

    Exercise 1 and 2

--- 01 - DOM-manipulation/js/exercices.js
+++ 01 - DOM-manipulation/js/exercices.js
@@ -1,1 +1,53 @@
-// Your code goes here
+/* EXERCISE 1*/
+
+/* Injecter text dans l'ID paragraph via innerHTML */
+document.getElementById("paragraph").innerHTML = "Hello World!";
+
+/* injection dans le dom via une fonction*/ 
+let paragraph = document.querySelector('#paragraph')
+
+function addTextInside(cible, contenu){
+    cible.innerText = contenu
+}
+
+addTextInside(paragraph, "bonjour DOM");
+
+/* Changer image via .src */
+document.getElementById("image").src="/images/test2.png";
+
+/* Creation du bouton */
+let newButton = document.createElement('button');
+
+/* creation de l'ID du bouton */
+newButton.id = 'button';
+/* Injection du bouton dans le div en tant que enfant */
+document.getElementById('div').appendChild(newButton);
+
+/* Creation du text dans le bouton */
+let newButtonText = document.createTextNode('Cliquez ici!');
+
+/* injection du text dans le bouton */ 
+newButton.appendChild(newButtonText);
+
+/* Selecting the class, adding new class name and remove class name */
+const para = document.querySelector('.para');
+para.classList.add('newPara');
+para.classList.remove("para");
+
+/* Creation du liste des noms */
+let list = ['alice', 'bob', 'charlie'];
+
+/* Creation of the for loop, create li, add name into li, select ul, create child of ul */
+for (i = 0; i < list.length; i++)   {
+let li = document.createElement('li');
+li.innerHTML = list[i];
+document.querySelector('ul').append(li);
+}
+
+/* Remove an element via ID */
+const liToRemove = document.getElementById('3');
+liToRemove.remove();
+
+/* Hide an element with Display: none */
+document.getElementById("2").style.display = "none";
+
--- 02 - events-management/js/exercices.js
+++ 02 - events-management/js/exercices.js
@@ -1,1 +1,77 @@
-// Your code goes here
+/* Creation of the variable button via ID */
+let button = document.getElementById('button');
+
+/* Adding eventListener ("click"), and adding a function on click, (alert) */
+button.addEventListener("click", function(){ alert("You clicked! :) "); }); 
+
+/* Get the element and stock it in the variable */
+const elementHover = document.getElementById('hover');
+
+/* Add eventlistener on mouseenter and mouseleave (hover) to change the color of the text */
+elementHover.addEventListener('mouseenter', function() {  elementHover.style.color = '#e74c3c';});
+elementHover.addEventListener('mouseleave', function() {  elementHover.style.color = 'black';});
+
+/* Get the input via QuerySelector */
+const input = document.querySelector("input");
+
+/* Add eventListener keydown, function logKey to get the key pressed */
+input.addEventListener("keydown", logKey);
+
+/* Log the key pressed in the console */
+function logKey(e) {
+  console.log(e.code);
+}
+
+/* Get the button and the dialog via ID */
+const buttonShow = document.getElementById('my-button');
+const dialog = document.getElementById('my-dialog');
+
+/* Add eventlistener on click to the button to open Modal */
+buttonShow.addEventListener('click', () => {dialog.showModal();});
+
+/* Close dialog if click is different then click on my-div */
+dialog.addEventListener('click', (event) => {
+    if (event.target.id !== 'my-div') {
+        dialog.close();
+    }
+});
+
+/* Validating the input of the form, to see if the form is empty or not */
+function validateForm() {
+    /* Get the input from the form and store it in a variable */
+    let nameInput = document.forms["form"]["name"].value;
+    let emailInput = document.forms["form"]["email"].value;
+    let phoneInput = document.forms["form"]["phonenumber"].value;
+
+    /* check if the field is empty, if so return a alert and false to stop sending the form */
+    if (nameInput == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+    if (emailInput == "")   {
+        alert('Email must be filled out');
+        return false;
+    }
+    if (phoneInput == "")   {
+        alert('You need to give a phone number')
+        return false;
+    }
+  } 
+
+/* Code to check the forms simplified */
+
+function validateForm() {
+    const form = document.forms["form"];
+    const inputs = ["name", "email", "phonenumber"];
+
+    for (const input of inputs) {
+        if (form[input].value === "") {
+            alert(`${input.charAt(0).toUpperCase() + input.slice(1)} must be filled out`);
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
